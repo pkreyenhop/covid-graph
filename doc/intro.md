479df0491eb315368a6f7461509445c2cf44a7c2
Peter Kreyenhop <Peter.Kreyenhop@gmail.com>
2020-06-04 10:35:52 +1200

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to mygraph
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/mygraph/core.clj
@@ -0,0 +1,52 @@
+(ns mygraph.core
+  (:gen-class))
+
+(declare create-corona-chart-sample-call)
+
+(require '[com.hypirion.clj-xchart :as c])
+(require '[cheshire.core :refer :all])
+(import '(java.io ByteArrayInputStream))
+
+
+
+(defn -main
+  [& args]
+  (create-corona-chart-sample-call))
+
+(defn read-covid-https-data
+  []
+  (parse-stream (clojure.java.io/reader "https://pomber.github.io/covid19/timeseries.json")))
+
+(defn find-country-data
+  [country]
+  "bla"
+  (get (read-covid-https-data) country))
+
+(defn find-property
+  [country slot]
+  (for [x (find-country-data country)] (get x slot)))
+
+(defn create-corona-chart
+  [country property]
+  (c/xy-chart {(str country "-" property)
+               [(range (count (find-property country property)))
+                (vec (find-property country property))]}))
+
+
+(defn add1 [n]
+  (+ 1 n))
+
+(defn create-corona-chart-nz
+  []
+  (c/spit (create-corona-chart "New Zealand" "confirmed") "nz.jpg"))
+
+(defn create-corona-chart-sweden
+  []
+  (c/spit (create-corona-chart "Sweden" "confirmed") "sweden.jpg"))
+
+
+(create-corona-chart-sweden )
+(create-corona-chart-nz)
+
+
+
